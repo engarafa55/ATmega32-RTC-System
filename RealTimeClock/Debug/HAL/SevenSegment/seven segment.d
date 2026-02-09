493c5565d938ad2f0c62e034b01d84dfdd3db4d1
Abdelrahman Arafa <engarafa55@gmail.com>
2026-02-09 20:25:14 +0200

    chore: Add .gitignore and remove build artifacts

--- RealTimeClock/Debug/HAL/SevenSegment/seven segment.d
+++
@@ -1,8 +0,0 @@
-HAL/SevenSegment/seven segment.d HAL/SevenSegment/seven segment.o: \
- ../HAL/SevenSegment/seven\ segment.c \
- ../HAL/SevenSegment/seven\ segment.h \
- ../HAL/SevenSegment/../../MCAL/DIO/DIO.h
-
-../HAL/SevenSegment/seven\ segment.h:
-
-../HAL/SevenSegment/../../MCAL/DIO/DIO.h:
